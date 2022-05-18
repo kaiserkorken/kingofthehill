045a173b785e646d8a114352ae381124df57005a
HerrTumorius <84377096+HerrTumorius@users.noreply.github.com>
2022-05-18 18:04:00 +0200

    added moves to tree function

--- src/game.py
+++ src/game.py
@@ -4,16 +4,36 @@ def turn(self, FEN):#ein kompletter zug der ki
     bb=FENtoBit(FEN)
     tree=tree(bb)#leerer baum mit b als root
     if not checkmate(FEN,player):#Spielende überprüfen
-        moves=player.generatemoves(bb)#liste aller moves
+        arr=set_movetree(tree,0,0,0)#arr=[tree,h,index,step]
+        while(time):#solange zeit ist
+            for z in range(arr[3], arr[2]):#eine weitere ebene durchgehen
+                arr=set_movetree(self,tree,arr[1],z)#ein ausgerechneter zug alle züge ausrechnen
+    
         #utility auf root?
-        for x in moves:#outsourcen
-            tree.insert_node([x,player.utility(x)])#in Node mit bitboard und wertung einsetzen
         tree=player.alphabetasearch(tree)#indizes aktualisieren
         index=player.best_node(tree)#besten zug auswaehlen
         move=tree.findNode(index)
         bb=player.make_move(move)
         FEN=BittoFEN(bb)
         return FEN
+
+
+def set_movetree(self,tree,h=0,index=0,step=0):#added züge zum baum, solange time = True
+    #while (time-(tmax/2)>0):
+    
+    b=tree.find_node(index)
+    while b.h==h:
+        moves=player.generatemoves(b.value)#liste aller moves von bb
+        for x in moves:
+            self.insert_node(b,[x,player.utility(x),h])  #in Node mit bitboard und wertung einsetzen
+        index +=1
+        b=tree.find_node(index)
+    step+=index
+    h+=1
+
+    
+    return tree,h,index, step
+
 def checkmate(FEN,player):#Spiel nächsten Zug beendet -> True
     b=BittoFEN(FEN)
     #TODO b-> Spiel beendet?
