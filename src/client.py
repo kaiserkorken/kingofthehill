93276973decfb2d677417852118dc832fdf84fda
AlexanderCalli <51166223+AlexanderCalli@users.noreply.github.com>
2022-05-22 12:26:19 +0200

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,22 +1,38 @@
 import threading
 import socket
-import time
-
 alias = input('Choose an alias >>> ')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 59455))
+client.connect(('127.0.0.1', 59566))
 
-#Mögliche Lösung, rückgabewert zu speichern
+
+
 def client_receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            counter = int(message)+1
             if message == "alias?":
                 client.send(alias.encode('utf-8'))
+            elif message =="1":
+                print("Sie spielen Weiß!")
+                est = "etwas"
+                client.send(est.encode('utf-8'))
+            elif message == "-1":
+                print("Sie spielen Schwarz!")
+                est = "etwas"
+                client.send(est.encode('utf-8'))
+                #client send
+    #        elif message == "Checkmate!":
+    #            print("You have won the game")
+    #            client.close()
+    #        elif message.split(" "[0]=="FEN"):
+    #            sgh = neuerMove(message.split(" ")[1],message.split(" ")[2])
+    #            client.send(sgh.encode('utf-8'))
+    #        elif checkmate(message.split(" ")[1],message.split(" ")[2]):
+    #            client.send(f'{alias}: has won the game!!')
+    #            client.close()
             else:
                 print(message)
-                return message
+
         except:
             print('Error!')
             client.close()
@@ -25,14 +41,8 @@ def client_receive():
 
 def client_send():
     while True:
-        msg = alias +" says: Test1"
-        message = msg.encode('utf-8')
-        msg_length = len(message)
-        send_length = str(msg_length).encode('utf-8')
-        send_length += b' ' * (1024 - len(send_length))
-        client.send(send_length)
-        client.send(message)
-        time.sleep(1)
+        message = input("")
+        client.send(message.encode('utf-8'))
 
 
 receive_thread = threading.Thread(target=client_receive)
@@ -41,3 +51,8 @@ receive_thread.start()
 send_thread = threading.Thread(target=client_send)
 send_thread.start()
 
+def neuerMove(bitbrd,player):
+    pass
+
+def checkmate():
+    pass
