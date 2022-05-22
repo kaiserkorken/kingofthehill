0d0b540da0f40aef3d76d3de3bd5a63d20da6d40
AlexanderCalli <51166223+AlexanderCalli@users.noreply.github.com>
2022-05-22 14:53:12 +0200

    Update client.py
    # spielerunterscheidung
    # FENTOBIT unterscheidung
    # Schachunterscheidung

--- src/client.py
+++ src/client.py
@@ -1,10 +1,12 @@
 import threading
 import socket
+from player import *
 alias = input('Choose an alias >>> ')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 59566))
-
-
+startFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+player = Player()
+win = True
 
 def client_receive():
     while True:
@@ -14,22 +16,22 @@ def client_receive():
                 client.send(alias.encode('utf-8'))
             elif message =="1":
                 print("Sie spielen Weiß!")
-                est = "etwas"
-                client.send(est.encode('utf-8'))
+                player.current=1
+
             elif message == "-1":
                 print("Sie spielen Schwarz!")
-                est = "etwas"
-                client.send(est.encode('utf-8'))
-                #client send
-    #        elif message == "Checkmate!":
-    #            print("You have won the game")
-    #            client.close()
-    #        elif message.split(" "[0]=="FEN"):
-    #            sgh = neuerMove(message.split(" ")[1],message.split(" ")[2])
-    #            client.send(sgh.encode('utf-8'))
-    #        elif checkmate(message.split(" ")[1],message.split(" ")[2]):
-    #            client.send(f'{alias}: has won the game!!')
-    #            client.close()
+                player.current=-1
+            elif FENtoBit(message):
+                if not checkmate(message,player):
+                    turn = player.turn(message)
+                    client.send(turn).encode('utf-8')
+                elif checkmate(message,player):
+                    win = False
+                    client.send(f'{alias}: has lost the game!!'.encode('utf-8'))
+                    client.close()
+            elif message.split(" ")[2] == "lost":
+                if win:
+                    print(f'{alias}: has won the game!!')
             else:
                 print(message)
 
@@ -41,18 +43,19 @@ def client_receive():
 
 def client_send():
     while True:
-        message = input("")
+        bsp= "9999888766"
+        message = bsp
+        time.sleep(1)
         client.send(message.encode('utf-8'))
 
 
 receive_thread = threading.Thread(target=client_receive)
 receive_thread.start()
 
-send_thread = threading.Thread(target=client_send)
-send_thread.start()
+#send_thread = threading.Thread(target=client_send)
+#send_thread.start()
 
-def neuerMove(bitbrd,player):
-    pass
+# spielerunterscheidung
+# FENTOBIT unterscheidung
+# Schachunterscheidung
 
-def checkmate():
-    pass
